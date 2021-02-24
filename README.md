cd7ce730a96330da74d581dc45f8e2ccbad5e897
Jonathan Jørgensen <jona.jorg@gmail.com>
2021-02-24 14:18:49 +0100

    Setup travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/CogitoNTNU/bombots.svg?branch=main)](https://travis-ci.org/CogitoNTNU/bombots)
+
 # bombots
 AI Tournament Environment, inspired by Pommerman
--- env.py
+++ env.py
@@ -75,7 +75,7 @@ bombs = []
 fires = []
 destruction_buffer = []
 fire_destruction_buffer = []
-
+"""
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -119,3 +119,4 @@ while True:
 
     pg.display.flip()
     clock.tick(int(framerate))
+"""
